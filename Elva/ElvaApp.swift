294924b965aa0fe077f5fc36e52d1fc81c125d85
Arthur Guiot <arguiot@gmail.com>
2022-06-01 10:52:12 +0800

    Initial Commit

---
+++ Elva/ElvaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvaApp.swift
+//  Elva
+//
+//  Created by Arthur Guiot on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ElvaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
